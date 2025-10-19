73cf3b759b9998e3fd1506f014790cbc3bc540dc
Mohamed Elhalees <melhalees@outlook.com>
2025-10-19 04:58:38 +0300

    Create 776-NAryTreePostorderTraversal

---
+++ 776-NAryTreePostorderTraversal/776-NAryTreePostorderTraversal.java
@@ -0,0 +1,39 @@
+// Last updated: 10/19/2025, 4:58:37 AM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans);
+        return ans;
+    }
+
+    private void dfs(Node root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+
+        for (Node child : root.children) {
+            dfs(child, ans);
+        }
+
+        ans.add(root.val);
+    }
+}
