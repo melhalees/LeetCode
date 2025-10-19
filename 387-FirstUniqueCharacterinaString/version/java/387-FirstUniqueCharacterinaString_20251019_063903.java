0348bc513885b0206c15e00d5f17fa0b0a6a8040
Mohamed Elhalees <melhalees@outlook.com>
2025-10-19 06:39:03 +0300

    Create 387-FirstUniqueCharacterinaString

---
+++ 387-FirstUniqueCharacterinaString/version/java/387-FirstUniqueCharacterinaString_20251019_063903.java
@@ -0,0 +1,18 @@
+// Last updated: 10/19/2025, 6:39:03 AM
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
