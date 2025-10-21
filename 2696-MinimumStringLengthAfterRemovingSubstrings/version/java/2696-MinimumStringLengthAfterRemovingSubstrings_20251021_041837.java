c2331cf6f7992af793053a9de4fa22445a98df72
Mohamed Elhalees <melhalees@outlook.com>
2025-10-21 04:18:38 +0300

    Create 2696-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2696-MinimumStringLengthAfterRemovingSubstrings/version/java/2696-MinimumStringLengthAfterRemovingSubstrings_20251021_041837.java
@@ -0,0 +1,23 @@
+// Last updated: 10/21/2025, 4:18:37 AM
+class Solution {
+    public int minLength(String s) {
+        char arr[] = s.toCharArray();
+        int n = arr.length;
+        int ind = -1;
+        for(int i = 0; i < n; i++) {
+            char ch = arr[i];
+            if(ind == -1) {
+                ind++;
+                arr[ind] = ch;
+            }
+            else if(ch == 'B' && arr[ind] == 'A') ind--;
+            else if(ch == 'D' && arr[ind] == 'C') ind--;
+            else {
+                ind++;
+                arr[ind] = ch;
+            }
+        }
+        return ind + 1;
+
+    }
+}
