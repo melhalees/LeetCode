e9335601db513416273859921d8b4d2897f487dc
Mohamed Elhalees <melhalees@outlook.com>
2025-10-24 06:15:02 +0300

    Create 1038-BinarySearchTreetoGreaterSumTree

---
+++ 1038-BinarySearchTreetoGreaterSumTree/version/java/1038-BinarySearchTreetoGreaterSumTree_20251024_061502.java
@@ -0,0 +1,30 @@
+// Last updated: 10/24/2025, 6:15:02 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root, 0);
+        return root;
+    }
+
+    private int dfs(TreeNode node, int sum) {
+        if (node == null) return sum;
+        sum = dfs(node.right, sum);
+        node.val += sum;
+        sum = node.val;
+        return dfs(node.left, sum);
+    }
+}
