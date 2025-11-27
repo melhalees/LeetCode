911437aa2e846aca6b87858d6c384ae8b234b0c8
Mohamed Elhalees <melhalees@outlook.com>
2025-11-27 19:42:20 +0300

    Create 2469-ConverttheTemperature

---
+++ 2469-ConverttheTemperature/version/java/2469-ConverttheTemperature_20251127_194220.java
@@ -0,0 +1,6 @@
+// Last updated: 11/27/2025, 7:42:20 PM
+1class Solution {
+2    public double[] convertTemperature(double celsius) {
+3        return new double[] {celsius + 273.15, celsius * 1.80 + 32.00};
+4    }
+5}
