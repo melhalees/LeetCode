0308ef967a7fa4533b8510a8c5298420a64ea082
Mohamed Elhalees <melhalees@outlook.com>
2025-11-28 20:01:28 +0300

    Create 853-CarFleet

---
+++ 853-CarFleet/version/java/853-CarFleet_20251128_200127.java
@@ -0,0 +1,18 @@
+// Last updated: 11/28/2025, 8:01:27 PM
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        float[] time = new float[target+1];
+        for (int i = 0; i < speed.length; i++) {
+            time[position[i]] = (float)(target-position[i])/speed[i];
+        }
+        int count = 0;
+        float maxi = 0;
+        for (int i = time.length - 1; i >= 0; i--) {
+            if (time[i] > maxi) {
+                count++;
+                maxi = time[i];
+            }
+        }
+        return count;
+    }
+}
