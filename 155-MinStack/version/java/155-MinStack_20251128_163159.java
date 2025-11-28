bca51a9ce8924336ba5c085e4ec6bb78a0d59c4f
Mohamed Elhalees <melhalees@outlook.com>
2025-11-28 16:31:59 +0300

    Create 155-MinStack

---
+++ 155-MinStack/version/java/155-MinStack_20251128_163159.java
@@ -0,0 +1,35 @@
+// Last updated: 11/28/2025, 4:31:59 PM
+class MinStack {
+	private Node head;
+        
+    public void push(int x) {
+        if (head == null) 
+            head = new Node(x, x, null);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+        
+    private class Node {
+        int val;
+        int min;
+        Node next;
+            
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
